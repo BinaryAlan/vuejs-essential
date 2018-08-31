0f01911f18e0f6df03c2b57cea7788de5cbaf352
liaochao <76660421@qq.com>
2018-08-31 12:16:01 +0800

    chapter 7

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,14 @@
-export default [
-  {
+export default [{
     path: '/auth/register',
     name: 'Register',
-    component: () => import('@/views/auth/Register')
+    component: () =>
+      import ('@/views/auth/Register')
   },
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home')
+    component: () =>
+      import ('@/views/Home')
   },
   {
     path: '*',
@@ -16,23 +17,26 @@ export default [
   {
     path: '/auth/login',
     name: 'Login',
-    component: () => import('@/views/auth/Login')
+    component: () =>
+      import ('@/views/auth/Login')
   },
   {
     path: '/users/1/edit',
-    component: () => import('@/views/users/Edit.vue'),
-    children: [
-      {
+    component: () =>
+      import ('@/views/users/Edit.vue'),
+    children: [{
         path: '',
         name: 'EditProfile',
-        component: ()=> import('@/views/users/Profile.vue'),
-        meta: {auth: true}
+        component: () =>
+          import ('@/views/users/Profile.vue'),
+        meta: { auth: true }
 
       },
       {
         path: '/users/1/edit_avatar',
         name: 'EditAvatar',
-        component: ()=>import('@/views/users/Avatar.vue'),
+        component: () =>
+          import ('@/views/users/Avatar.vue'),
         meta: {
           auth: true
         }
@@ -40,7 +44,8 @@ export default [
       {
         path: '/users/1/edit_password',
         name: 'EditPassword',
-        component: ()=>import('@/views/users/Password.vue'),
+        component: () =>
+          import ('@/views/users/Password.vue'),
         meta: {
           auth: true
         }
@@ -50,21 +55,36 @@ export default [
   {
     path: '/articles/create',
     name: 'Create',
-    component:()=>import('@/views/articles/Create'),
+    component: () =>
+      import ('@/views/articles/Create'),
     meta: {
       auth: true
     }
 
   },
-  {
-    path: '/articles/:articleId/content',
-    name: 'Content',
-    component: ()=> import('@/views/articles/Content')
-  },
+
   {
     path: '/articles/:articleId/edit',
     name: 'Edit',
-    component: () => import('@/views/articles/Create'),
-    meta: { auth: true}
+    component: () =>
+      import ('@/views/articles/Create'),
+    meta: { auth: true }
+  },
+  {
+    path: '/:user',
+    component: () =>
+      import ('@/views/articles/Column'),
+    children: [{
+      path: '',
+      name: 'Column',
+      component: () =>
+        import ('@/views/articles/List')
+    },
+    {
+      path: '/articles/:articleId/content',
+      name: 'Content',
+      component: () =>
+        import ('@/views/articles/Content')
+    }]
   }
 ]
